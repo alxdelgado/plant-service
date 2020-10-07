09f41d076fb1cb9f029bf3f1a7c2be3fa95b65d3
Alex Delgado <delgadoalex@icloud.com>
2020-10-07 15:19:32 -0500

    added LoginForm

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,68 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
+import {
+    Paper,  
+    Grid, 
+    TextField, 
+    Button, 
+    FormControlLabel,
+    Checkbox
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Face, Fingerprint } from '@material-ui/icons';
 
-export default function Login() {
-
-    const divStyle = {
-        fontSize: "30px", 
-        marginTop: "30px",
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(2),
+    }, 
+    padding: {
+        padding: theme.spacing(2),
+        marginTop: "50px",
+        width: "100%"
     }
+})); 
+
+
+function Login() {
+
+const classes = useStyles();
+
     return (
-        <div style={Object.assign(divStyle)}>
-            Create Login Form
-            <br />
-            Implement auth funtionality
-            <br />
-        </div>
+        <Paper className={classes.padding}>
+            <div className={classes.margin}>
+                <Grid container spacing={8} alignItems="flex-end">
+                    <Grid item>
+                        <Face />
+                    </Grid>
+                    <Grid item md={true} sm={true} xs={true}>
+                        <TextField is="username" label="Username" type="email" fullWidth autoFocus required />
+                    </Grid> 
+                </Grid>
+                <Grid container spacing={8} alignItems="flex-end">
+                    <Grid item>
+                        <Fingerprint />
+                    </Grid>
+                    <Grid item md={true} sm={true} xs={true}>
+                        <TextField is="password" label="Password" type="password" fullWidth required />
+                    </Grid>
+                </Grid>
+                <Grid container alignItems="center" justify="space-between">
+                    <Grid item>
+                        <FormControlLabel control={
+                            <Checkbox 
+                                color="primary"
+                            />
+                        } label="Remember me" />
+                    </Grid> 
+                    <Grid item>
+                        <Button style={{ textTransform: "none"}} variant="text" color="primary">Forgot Password ?</Button> 
+                    </Grid> 
+                </Grid>
+                <Grid>
+                    <Button variant="outlined" color="primary" style={{ textTransform: "none" }}>Login</Button>
+                </Grid>
+            </div>
+        </Paper>
     )
 };
+
+export default Login;
