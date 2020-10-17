eca77c510591999d831b90cc395b2df5c98670a7
Alex Delgado <delgadoalex@icloud.com>
2020-10-17 15:15:26 -0500

    added styling to the signup form --working on auth

--- src/components/Elements/Input.js
+++ src/components/Elements/Input.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        padding: "10px 0px 10px 0px"
+        padding: "10px 0px 0px 0px"
     }
 }));
 
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import Grid from '@material-ui/core/Grid';
 import { useForm, Form } from '../../components/Form/useForm';
 import Input from '../../components/Elements/Input';
+import Button from '../../components/Elements/Button';
 // import * as employeeService from '../../services/employeeService';
 
 // give default values for signUp form; 
@@ -16,12 +18,22 @@ const defaultValues = {
 }
 
 
-const SignUpGrid = styled.div`
+const SignUpContainer = styled.div`
     display: flex;
-    flex-direction: column;
-    width: 30%; 
+    justify-content: center;
+    width: 50%; 
     margin: 50px auto;
-    justify-content: space-between;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex; 
+    justify-content: center;
+    margin-left: 12rem;
+    width: 50%; 
+
+    @media screen and (min-width: 500px) {
+        body { background-color: yellow; }
+    }
 `;
 
 export default function SignUpPage() {
@@ -61,61 +73,73 @@ export default function SignUpPage() {
         }
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+    }
+
 
     return (
         <Form onSubmit={handleSubmit}>
-            <SignUpGrid>
-                <Input
-                    vairant="filled" 
-                    label="First Name"
-                    name="firstName"
-                    value={values.firstName}
-                    onChange={handleInputChange}
-                    error={errors.firstName}
-                />
-                <Input 
-                    label="Last Name"
-                    name="lastName"
-                    value={values.lastName}
-                    onChange={handleInputChange}
-                    error={errors.lastName}
-                />
-                <Input
-                    label="Email" 
-                    name="email"
-                    value={values.email}
-                    error={errors.email}
-                    onChange={handleInputChange}
-                />
-                <Input 
-                    label="Password"
-                    name="password"
-                    value={values.password}
-                    error={errors.password}
-                    onChange={handleInputChange}
-                />
-                <Input 
-                    label="City"
-                    name="city"
-                    value={values.city}
-                    error={errors.city}
-                    onChange={handleInputChange}
-                />
-                <Input 
-                    label="State"
-                    name="state"
-                    value={values.state}
-                    error={errors.state}
-                    onChange={handleInputChange}
-                />
-                <Input 
-                    label="Zipcode"
-                    name="zipCode"
-                    value={values.zipCode}
-                    error={errors.zipCode}
-                    onChange={handleInputChange}
-                />
-            </SignUpGrid>
+            <SignUpContainer>
+                <Grid xs={6}>
+                    <Input
+                        vairant="filled" 
+                        label="First Name"
+                        name="firstName"
+                        value={values.firstName}
+                        onChange={handleInputChange}
+                        error={errors.firstName}
+                    />
+                    <Input 
+                        label="Last Name"
+                        name="lastName"
+                        value={values.lastName}
+                        onChange={handleInputChange}
+                        error={errors.lastName}
+                    />
+                    <Input
+                        label="Email" 
+                        name="email"
+                        value={values.email}
+                        error={errors.email}
+                        onChange={handleInputChange}
+                    />
+                    <Input 
+                        label="Password"
+                        name="password"
+                        value={values.password}
+                        error={errors.password}
+                        onChange={handleInputChange}
+                    />
+                </Grid>
+                <Grid xs={6}>
+                    <Input 
+                        label="City"
+                        name="city"
+                        value={values.city}
+                        error={errors.city}
+                        onChange={handleInputChange}
+                    />
+                    <Input 
+                        label="State"
+                        name="state"
+                        value={values.state}
+                        error={errors.state}
+                        onChange={handleInputChange}
+                    />
+                    <Input 
+                        label="Zipcode"
+                        name="zipCode"
+                        value={values.zipCode}
+                        error={errors.zipCode}
+                        onChange={handleInputChange}
+                    />
+                </Grid>
+            </SignUpContainer>
+            <ButtonContainer>
+                <Button>Submit</Button>
+            </ButtonContainer>
         </Form>
     )
 }
